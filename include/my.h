3aedfacdd1a98caf13486c726912be6e9b83da94
Naira <naira.awadin@epitech.eu>
2023-05-27 16:18:28 +0200

    [ADD] structure for the menu

--- include/my.h
+++ include/my.h
@@ -11,4 +11,24 @@
 #ifndef MY_H_
     #define MY_H_
 
-#endif /* !MY_H_ */
+typedef struct my_rpg_s {
+    //sfTexture* texture;
+    //sfSprite* sprite;
+
+    sfTexture* text_button_play;
+    sfSprite* button_play;
+
+    sfTexture* text_button_setting;
+    sfSprite* button_setting;
+
+    sfTexture* text_button_exit;
+    sfSprite* button_exit;
+
+    sfTexture* text_button_credits;
+    sfSprite* button_credits;
+
+    sfSprite* sprites[31];
+
+}my_rpg;
+
+#endif
